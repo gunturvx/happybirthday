d7713fc5cd849fe4e740e66dc4f111886b21754f
gunturvx <63529533+gunturvx@users.noreply.github.com>
2020-09-26 10:52:29 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/SEjwrSd6Lww");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/Z61q32BTNHk");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
